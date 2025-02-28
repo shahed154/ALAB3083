8976dd2b011e359272150a8cba5c8d474d11449e
shahed154 <shahedadnan@hotmail.com>
2025-02-27 22:33:58 -0500

    commit

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-test
+
+// Part 1: Fizz Buzz
+let fizzBuzz = 0;
+
+for (i = 0; i <= fizzBuzz; i++) {
+
+    if (i % 5 == 0 && i % 3 == 0) {
+        console.log(i + " Fizz Buzz")
+    }
+    else if (i % 5 == 0) {
+        console.log(i + " Buzz")
+
+    }
+    else if (i % 3 == 0) {
+        console.log(i + " Fizz")
+
+    }
+    else !(i % 5 == 0 || i % 3 == 0)
+    {
+        console.log(i + " Not divisble by 3 or 5")
+    }
+}
+//console.log(n + " is a prime number")
+//Part 2: Prime Time
+//let maxNumberCheck = 5
+//for(n = 1; n <= maxNumberCheck; n++) {
+  // let currentNum = n; 
+  //  for(i = 2; i < Math.sqrt(currentNum); currentNum--){
+  //      if(i % currentNum == 0){
+         // console.log(n)
+   //     }
+   //     else
+    //    {
+   //     console.log(n)
+  //      }
+//
+   // }
+//}
+/// I GAVE UP AND HAD TO GOOGLE IT 
+
+let maxNumberCheck = 100
+let totalPrimeNumber = 0;
+for (let i = 1; i <= maxNumberCheck; i++) {
+  let prime = i > 1;
+  for (let j = 2; j <= Math.sqrt(i); j++) {
+    if (i % j == 0) {
+      prime = false;
+      break;
+    }
+  }
+  if (prime) {
+    console.log(i, "is a prime number");
+    totalPrimeNumber += 1;
+  } else {
+    console.log(i, "is not a prime number");
+  }
+}
---
+++ part3.js
@@ -0,0 +1,46 @@
+let CSV = ("ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26");
+
+//console.log(CSV)
+
+//let example = ("Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232")
+
+//console.log(example)
+
+
+{
+
+let totalLength = CSV.length;
+console.log(totalLength)
+
+//let cells = 
+//while (cells < length)
+{
+//cells--
+//console.log(cells)
+}
+}
+const str = ("ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26");
+
+
+// for (let i = 0; i < str.length; i++) {
+// 	if (str[i] == ",") {
+// 		continue;
+// 	}
+
+// 	console.log(str[i]);
+// }
+
+
+
+let x = 30;
+
+while (x > 0) {
+	x /= 2;
+	x--;
+
+	if (x % 1 != 0) {
+		break;
+	}
+	
+	console.log(x);
+}
