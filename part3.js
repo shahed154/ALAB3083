5af22975150d4f9776e7fb72216d81a14f6b5428
shahed154 <shahedadnan@hotmail.com>
2025-02-28 22:34:41 -0500

    test

--- part3.js
+++ part3.js
@@ -19,14 +19,25 @@ const str = ("ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63
 for (let i = 0; i < str.length; i++) {
 
 	console.log(str.slice(i, i + 1))
-	cell1 =(cell1.concat(str.slice(i,i+1)))
 
+	if(cell1 = ""){
+	cell1 =(cell1.concat(str.slice(i,i+1)))
+	}
+	else if(cell2 = ""){
+		cell2 =(cell2.concat(str.slice(i,i+1)))
+		}
+		else if(cell3 = ""){
+			cell3 =(cell3.concat(str.slice(i,i+1)))
+			}	else if(cell4 = ""){
+				cell4 =(cell4.concat(str.slice(i,i+1)))
+				}
 
 	if (str.slice(i, i + 1) == ',') {
 
-		
-	//	console.log("~~" + cell2)
-	break;
+		// cell1 = cell1.slice(0, -1)
+		console.log("~" + cell1 + "~" + cell2 + "~" + cell3+ "~" + cell4)
+
+	
 
 	}
 
